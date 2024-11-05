18f12d5195ea27164140211b434a4fa567a42729
Avinash <112330806+Uavina@users.noreply.github.com>
2024-11-05 16:47:25 +0530

    daily

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from './Components/Todo'
+
+const App = () => {
+  return (
+    <div className='bg-lime-300 grid py-4 h-screen'>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
+
+
